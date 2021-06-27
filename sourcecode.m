2e20f9810d911f64b55b7b9fff02953b0c28a699
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2021-06-27 22:14:45 +0200

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -6,14 +6,7 @@
 % http://matlab-recognition-code.com/gait-biometric-recognition-system-matlab-source-code/
 %
 % 
-% For any question please email me hamdouchhd@hotmail.com
 %
-% Hamdi Boukamcha
-% Tunisia
-% Sousse
-% 4081
-% email  hamdouchhd@hotmail.com
-% mobile +21650674269
 % website http://matlab-recognition-code.com
 %
 %
